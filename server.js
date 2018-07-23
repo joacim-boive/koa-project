de59c1d4a75efc4bba5c19b8f921d534ac6785a2
Joacim Boive <joacim.boive@gmail.com>
2018-07-23 14:00:10 +0200

    Simple proxy setup

--- server.js
+++ server.js
@@ -5,8 +5,26 @@ const proxy = require('http-proxy-middleware');
 const app = new Koa();
 
 app.use((convert(proxy({
-    'target': 'http://www.example.org/',
-    logLevel: 'debug'
+    target: 'https://www.ecster.se',
+    changeOrigin: true,
+    secure: true,
+    logLevel: 'debug',
+    headers:{
+        'my-header': 'this is my header'
+    },
+    onClose: (res, socket, head) =>{
+      console.info('Proxy closed!');
+    },
+    onError: (err, req, res) => {
+        console.error(err.message);
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        proxyRes.headers['x-onProxyRes'] = 'response';
+    },
+    onProxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('x-onProxyReq', 'request');
+    },
+
 }))));
 
 app.listen(3000);
